f7e9a5bb798678d71ef2b0a2bd98cf137926133a
Nathan <baumnathan@gmail.com>
2022-02-02 22:42:34 -0500

    favicon

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -9,6 +9,11 @@
     <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 </head>
 <body class="bg-white font-sans">
     @include('_partials.header')
