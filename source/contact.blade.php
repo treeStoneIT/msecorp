2ddeabe8266edadc1d51b70043799aaae7016eb4
Nathan <baumnathan@gmail.com>
2023-01-18 10:26:50 -0500

    change phone number

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -115,7 +115,7 @@ description: Thank you for your interest in Modern Sign & Engraving. Please cont
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                               d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
                                     </svg>
-                                    <a href="tel:+1-416-783-1487" class="ml-3">+1 (416) 783-1487</a>
+                                    <a href="tel:+1-416-668-0144" class="ml-3">+1 (416) 668-0144</a>
                                 </dd>
                                 <dt><span class="sr-only">Email</span></dt>
                                 <dd class="flex text-base text-gray-50">
